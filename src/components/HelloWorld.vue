06a21d920145b71d2ed3054dd3a09af46165b787
blentle <blentle@sina.com>
2017-11-24 18:30:17 +0800

    Add split & axio

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,30 @@
 <template>
   <div class="mri-warp">
     <div class="content-head">
-      <div style="flex: 1">
-
+      <div   class="head-left">
       </div>
-
-      <div style="flex: 3;padding-left: 20px;">
+      <div   class="head-right">
         <label>时间都拿手机的</label>
-        <i class="fa fa-save" style="float: right"></i>
-        <i class="fa fa-save" style="float: right"></i>
-        <i class="fa fa-save" style="float: right"></i>
-        <i class="fa fa-save" style="float: right"></i>
+        <button class="icon-btn">
+          <i class=" fa fa-save"  ></i>
+        </button>
+        <button class="icon-btn">
+          <i class=" fa fa-save"  ></i>
+        </button>
+        <button class="icon-btn">
+          <i class=" fa fa-save"  ></i>
+        </button>
+
       </div>
     </div>
     <div class="mri-content">
-      <div class="mri-view"></div>
-      <div class="tree-view">
+      <div class="mri-view" id="mri">
+        <iframe style="width: 100%;height: 100%;border: none;" src="https://m.baidu.com"></iframe>
+      </div>
+      <div class="tree-view" id="tree">
         <v-jstree :data="treeData" wholeRow  ></v-jstree>
       </div>
-      <div class="editor-view">
+      <div class="editor-view" id="editor">
         <json-editor v-model="json"  height="100%" lang="json" theme="monokai" @init="initEditor"></json-editor>
       </div>
     </div>
@@ -31,7 +37,7 @@
 import 'font-awesome/css/font-awesome.css'
 import JsonEditor from  "vue2-ace-editor"
 import VJstree from 'vue-jstree'
-
+import Split from 'split.js'
 export default {
   name: 'HelloWorld',
   data () {
@@ -56,6 +62,9 @@ export default {
             "opened":true,
             "children": [
               {
+                "text": "initially selected",
+                "icon":"fa fa-file-text-o",
+              },{
                 "text": "initially selected",
                 "icon":"fa fa-file-text-o",
               },
@@ -88,11 +97,24 @@ export default {
 
     }
   },
+  created:function(){
+    this.axios.get("https://api.github.com/repos/NiuShy/editor/contents")
+            .then(function(response) {
+              console.log(response.data);
+            });
+
+
+  },
   methods:{
     initEditor:function (editor) {
       require('brace/mode/json');
       require('brace/theme/monokai');
     }
+  },
+  mounted:function(){
+    Split(['#tree', '#editor'], {
+      sizes: [25, 75]
+    })
   },
   components: {
     VJstree,
@@ -103,9 +125,8 @@ export default {
 
 <style >
 .mri-content{
-  height: 500px;
+  height: 700px;
   display: flex;
-
 }
 .mri-warp .content-head{
   height: 30px;
@@ -115,25 +136,66 @@ export default {
   vertical-align: middle;
   display: flex;
 }
+.mri-warp .content-head .head-left{
+  width: 420px;
+}
+.mri-warp .content-head .head-right{
+  width: calc(100% - 70px);
+ }
+.mri-warp .content-head .head-right .icon-btn{
+  float: right;
+  color: #fff;
+  background-color: #409eff;
+  border-color: #409eff;
+  border: 1px solid #d8dce5;
+  display: inline-block;
+  line-height: 1;
+  white-space: nowrap;
+  cursor: pointer;
+  -webkit-appearance: none;
+  text-align: center;
+  box-sizing: border-box;
+  outline: none;
+  margin: 0;
+  transition: .1s;
+  font-weight: 500;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  padding: 8px 16px;
+  font-size: 14px;
+  border-radius: 4px;
+  margin-left: 10px;
+}
+.mri-warp .content-head .head-right .icon-btn:hover{
+  opacity: .8;
+  filter:alpha(opacity=80);
+}
+.mri-warp .content-head .head-right .icon-btn:active{
+  opacity: 1;
+  filter:alpha(opacity=100);
+}
 .mri-content>div{
   border: 1px solid #e6ebf5;
-
   overflow: hidden;
   box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
-
 }
 .mri-content .mri-view{
-  flex: 1;
+  width: 420px;
   padding: 10px;
 }
 .mri-content .tree-view{
-  flex: 1;
+ /*flex: 1;*/
   overflow: hidden;
   overflow-x: scroll;
+  overflow-y: scroll;
   padding: 10px;
  }
 .mri-content .editor-view{
-  flex: 2;
+  /*flex: 2;*/
 }
-
+  .gutter.gutter-horizontal{
+    border: none;
+    cursor: w-resize;
+  }
 </style>
